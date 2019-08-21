aad04c810dcc2dd07f31e625acca921148daad5f
fsbalbuena <fsbalbuena@gmail.com>
2019-08-21 11:05:25 -0300

    Ready the UI, now need destructuring

--- front-end/frontend-challenge/src/components/AllCountries.jsx
+++ front-end/frontend-challenge/src/components/AllCountries.jsx
@@ -1,46 +1,101 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Chip from '@material-ui/core/Chip';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+
 import {Link} from 'react-router-dom' 
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import { Grid } from '@material-ui/core';
 
-export default ()=>{
+const useStyles = makeStyles(theme => ({
+  title:{
+    textAlign:"center",
+    padding:theme.spacing(3)
+  },
+  languageTitle:{
+    paddingBottom:theme.spacing(2)
+  },
+  card: {
+    maxWidth: "80%",
+    margin:"0 auto"
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  chip:{
+    
+  }
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
   const { loading, error, data } = useQuery(gql`
   {
     countries{
-    code,
-    name,
-    currency,
-    native,
-    languages{
-    name,
-    native
-    },
-    continent{
-    name
-    }
-    }
-    }
+              code,
+              name,
+              currency,
+              native,
+              languages{
+                        name,
+                        native
+                        },
+              continent{
+                        name
+                        }
+            }
+  }
   `);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-
+  
   return (
-    <table style={{width:"80%"}}>
-    <tr>
-      <th>Name</th>
-      <th>Lenguages</th>
-    </tr>
+    <>
+    <Typography variant="h1" color="textSecondary" className={classes.title}>
+          Countries
+    </Typography>
     {data.countries.map(country=>(
-        <tr>
-        <td><Link to={`/countries/${country.code}`}>{`${country.name}/${country.native}`}</Link></td>
-        <td>
-            <ul>
-                {country.languages?country.languages.map(language=><li>{`${language.name}/${language.native}`}</li>):null}
-            </ul>
-        </td>
-      </tr>
+      <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {country.code}
+          </Avatar>
+        }
+        title={<Typography variant="h4" component={Link} to={`/countries/${country.code}`} color="textSecondary">
+        {country.name}
+      </Typography>}
+        subheader={<Typography variant="h6" color="textSecondary">
+        {country.native}
+      </Typography>}
+      />
+      <CardContent>
+        <Typography variant="h5" color="textSecondary" className={classes.languageTitle}>
+          Languages:
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          >
+          {country.languages.map(language=>(
+            <Grid item xs={12} sm={6} md={4}>
+            <Chip label={`${language.name} -${language.native} `} className={classes.chip} color="primary"/>
+          </Grid>
+          ))}
+
+        </Grid>
+      </CardContent>
+    </Card>
     ))}
-  </table>
-  )
+    </>
+  );
 }
