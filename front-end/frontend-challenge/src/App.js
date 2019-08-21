7424fc246030921aafd9cae3586781e86885090a
fsbalbuena <fsbalbuena@gmail.com>
2019-08-21 08:51:10 -0300

    Navbar component created on raw jsx only functionality purposes

--- front-end/frontend-challenge/src/App.js
+++ front-end/frontend-challenge/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div >
-      <h1>Hola</h1>
+      <Navbar />
+      <h1>Countries</h1>
     </div>
   );
 }
---
+++ front-end/frontend-challenge/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default ()=>(
+
+    <nav className="navbar">
+        <div className="navbar-brand">
+        <img className="navbar-img" src="http://www.orxeta.es/wp-content/uploads/2018/07/Idiomas.png" alt=""/>
+        <span >App</span>
+        </div>
+        <span className="navbar-link-right">Countries</span>
+    </nav>
+)
