89a967b3e8512205609d2782999f020cfb693bec
fsbalbuena <fsbalbuena@gmail.com>
2019-08-21 09:31:49 -0300

    CurrentCountry consumes the info

--- front-end/frontend-challenge/src/App.js
+++ front-end/frontend-challenge/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import AllCountries from './components/AllCountries'
+import CurrentCountry from './components/CurrentCountry'
 
 function App() {
   return (
     <div >
       <Navbar />
+      <CurrentCountry />
       <AllCountries/>
     </div>
   );
